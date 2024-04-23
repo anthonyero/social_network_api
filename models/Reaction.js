6d8ba3ffeb27f73644f6ff685379c4b388ae996a
anthonyero <anthonyero@gmail.com>
2024-04-23 00:57:44 -0500

    Imported the format_date helper function into this Reaction schema

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,4 +1,5 @@
 const { Schema } = require('mongoose');
+const { format_date } = require('../utils/helpers');
 
 const reactionSchema = new Schema(
 	{
@@ -17,7 +18,8 @@ const reactionSchema = new Schema(
 		},
 		createdAt: {
 			type: Date,
-			default: Date.now
+			default: Date.now,
+			get: format_date
 		}
 	},
 	{
