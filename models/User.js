0ca63818be3475e340047c2e8e924b8d3bb5a55f
anthonyero <anthonyero@gmail.com>
2024-04-23 01:47:43 -0500

    Defined a User route with email validation and a virtual to retrieve the length of the friends array

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+	{
+		username: {
+			type: String,
+			unique: true,
+			required: true,
+			trim: true
+		},
+		email: {
+			type: String,
+			unique: true,
+			required: true,
+			match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, "Please provide a valid email"]
+		},
+		thoughts: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: 'thought'
+			}
+		],
+		friends: [
+			{
+				type: String.Types.ObjectId,
+				ref: 'user'
+			}
+		]
+	},
+	{
+		toJSON: {
+			virtuals: true,
+			getters: true
+		}
+	}
+);
+
+userSchema.virtuals(friendCount).get(function() {
+  return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
