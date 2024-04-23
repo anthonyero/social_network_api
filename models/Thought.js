68b226b40ac2b36492aadb7fdc8f3a1f6651d19c
anthonyero <anthonyero@gmail.com>
2024-04-23 01:15:07 -0500

    Imported model from mongoose

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,4 @@
-const { Schema } = require('mongoose');
+const { Schema, model } = require('mongoose');
 const { format_date } = require('../utils/helpers');
 const reactionSchema = require('./Reaction');
 
