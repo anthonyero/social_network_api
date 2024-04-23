6d154c2d43e3f410f897405bd5bd86219042d7e5
anthonyero <anthonyero@gmail.com>
2024-04-23 17:23:56 -0500

    Defined sample user, thought, and reactions data

---
+++ utils/data.js
@@ -0,0 +1,67 @@
+const usersData = [
+	{
+		username: 'johnsmith',
+		email: 'john@email.com'
+	},
+	{
+		username: 'janedoe',
+		email: 'jane@email.com'
+	},
+	{
+		username: 'angeliaeber',
+		email: 'angelia@email.com'
+	},
+	{
+		username: 'marillacosme',
+		email: 'marilla@email.com'
+	},
+	{
+		username: 'janrunar',
+		email: 'jan@email.com'
+	},
+	{
+		username: 'chandrajasmine',
+		email: 'chandra@email.com'
+	}
+]
+
+const thoughtsData = [
+	{ 
+		thoughtText: 'Very excited to learn REACT',
+		username: 'johnsmith'
+	},
+	{
+		thoughtText: `I'm using a NoSQL database for this social network API.`,
+		username: 'johnsmith'
+
+	},
+	{
+		thoughtText: 'A new social network?',
+		username: 'janedoe'
+	},
+	{
+		thoughtText: 'Cannot wait to travel',
+		username: 'angeliaeber'
+	},
+	{
+		thoughtText: 'Am I using this site correctly?',
+		username: 'marillacosme'
+	},
+	{
+		thoughtText: 'Chicago is my favorite city',
+		username: 'janrunar'
+	},
+	{
+		thoughtText: 'I hope we have weather this nice forever',
+		username: 'chandrajasmine'
+	}
+]
+
+const reactionsData = [
+	'Agreed',
+	'Same',
+	'Boo',
+	'Cannot believe it',
+	'Strongly disagree',
+	'Delete the app'
+]
