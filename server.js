7c577878fb323066800975ebcbda7cb85771000d
anthonyero <anthonyero@gmail.com>
2024-04-23 22:17:30 -0500

    Imported the routes `index.js` file and passed these routes as middleware

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const db = require('./config/connection');
-// Import routes here when they can be imported
+const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001; // If deployed remotely, PORT environment variable will be useful
 const app = express();
@@ -8,7 +8,7 @@ const app = express();
 // Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
-// When routes are implemented pass, routes as middleware
+app.use(routes);
 
 db.once('open', () => {
 	app.listen(PORT, () => {
