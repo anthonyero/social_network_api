685b1936e4c24a0c0e64e1c7bd3722ab979efe17
anthonyero <anthonyero@gmail.com>
2024-04-24 01:10:48 -0500

    Defined a getSingleThought function and imported it as a GET request at the /api/thoughts/:thoughtId endpoint

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -11,4 +11,16 @@ module.exports = {
 			res.status(500).json(err);
 		}
 	},
+
+	// Get a specific thought by its `_id`
+	async getSingleThought(req, res) {
+		try {
+			const user = await Thought.findOne({ _id: req.params.thoughtId})
+			.select('-__v')
+			.populate({ path: 'reactions', select: '-__v'});
+			res.status(200).json(user)
+		} catch (err) {
+			res.status(500).json(err)
+		}
+	},
 }
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,11 +1,15 @@
 const router = require('express').Router();
 const {
 	getThoughts,
-
+	getSingleThought
 } = require('../../controllers/thoughtController.js');
 
 // Endpoint: `/api/thoughts`
 router.route('/')
 	.get(getThoughts)
+
+// Endpoint: `/api/thoughts/:thoughtId`
+router.route('/:thoughtId')
+	.get(getSingleThought)
 
 module.exports = router;
