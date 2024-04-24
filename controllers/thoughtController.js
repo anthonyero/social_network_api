2cfc01ce1364555d931e883ac213acf62f18ccab
anthonyero <anthonyero@gmail.com>
2024-04-24 01:47:23 -0500

    Defined a createNewThought function that also updates a users thoughts array with the newly generated thought ID. Imported into thoughtRoutes.js

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,12 +1,14 @@
 const { User, Thought } = require('../models');
+const { ObjectId } = require('mongoose').Types;
+
 
 module.exports = {
 	// Get all thoughts
 	async getThoughts(req, res) {
 		try {
-			const users = await Thought.find()
+			const thoughts = await Thought.find()
 				.select('-__v');
-			res.status(200).json(users);
+			res.status(200).json(thoughts);
 		} catch (err) {
 			res.status(500).json(err);
 		}
@@ -15,12 +17,29 @@ module.exports = {
 	// Get a specific thought by its `_id`
 	async getSingleThought(req, res) {
 		try {
-			const user = await Thought.findOne({ _id: req.params.thoughtId})
+			const thought = await Thought.findOne({ _id: req.params.thoughtId})
 			.select('-__v')
 			.populate({ path: 'reactions', select: '-__v'});
-			res.status(200).json(user)
+			res.status(200).json(thought);
 		} catch (err) {
-			res.status(500).json(err)
+			res.status(500).json(err);
+		}
+	},
+
+	// Create a new thought
+	async createNewThought(req, res) {
+		try {
+			const thoughtData = await Thought.create(req.body);
+
+			// We want to also add this new thought to the User's document
+			const updateUser = await User.findOneAndUpdate(
+				{ $or: [{_id: req.body.userId}, {username: req.body.username}]}, // Matches based on username, but if a user passes their userID that will match first
+				{ $addToSet:  { thoughts: new ObjectId(thoughtData) }},
+				{ runValidators: true, new: true }
+			);
+			res.status(201).json(thoughtData);
+		} catch (err) {
+			res.status(500).json(err);
 		}
 	},
 }
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 const {
 	getThoughts,
-	getSingleThought
+	getSingleThought,
+	createNewThought
 } = require('../../controllers/thoughtController.js');
 
 // Endpoint: `/api/thoughts`
 router.route('/')
 	.get(getThoughts)
+	.post(createNewThought)
 
 // Endpoint: `/api/thoughts/:thoughtId`
 router.route('/:thoughtId')
