0be9896de105c34222c8bcacbca7d094a43bafa5
anthonyero <anthonyero@gmail.com>
2024-04-23 20:13:30 -0500

    Corrected an error where I placed `virtuals` instead of `virtual` in the models and also placed parentheses around the virtual key name

--- models/User.js
+++ models/User.js
@@ -22,7 +22,7 @@ const userSchema = new Schema(
 		],
 		friends: [
 			{
-				type: String.Types.ObjectId,
+				type: Schema.Types.ObjectId,
 				ref: 'user'
 			}
 		]
@@ -35,7 +35,7 @@ const userSchema = new Schema(
 	}
 );
 
-userSchema.virtuals(friendCount).get(function() {
+userSchema.virtual('friendCount').get(function() {
   return this.friends.length;
 });
 
